3f6c94d662b54972620cc3a3ffc65f6df1e62d89
DarkDemon135 <122680581+DarkDemon135@users.noreply.github.com>
2023-01-23 17:45:49 +0000

    Main

---
+++ work.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+class Drob {
+    int a,b;
+
+    public:
+    Drob() : Drob(0,0) {}
+
+    Drob(const Drob &other) : Drob(other.a, other.b) {}
+
+    Drob(int a, int b) {
+        this->a = a;
+        this->b = b;
+    }
+
+    void print() {
+        std::cout << this->a << "/" << this->b << "\n";
+    }
+
+    void input() {
+        std::cout << "Input a: ";
+        std::cin >> a;
+        std::cout << "Input b: ";
+        std::cin >> b;
+
+    }
+
+    void plus(int val) {
+        val = val * this->b;
+
+        this->a = this->a + val;
+    }
+};
+
+int main(int argc, char const *argv[]) 
+{
+    Drob test;
+
+    test.input();
+    Drob copy = test;
+
+    test.print();
+    test.plus(4);
+    test.print();
+    return 0;
+}
